b20d5d26c894217f91d76eccd2fb2ee6b7921ba8
Vojta Havle <vojtech.havle@gmail.com>
2025-07-13 13:35:49 +0200

    korekce textu

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -39,26 +39,33 @@ function LandingPage() {
         src="/images/úvod.png"
         alt="Úvodní obrazek"
         style={{ maxWidth: '100%', height: 'auto' }} />
-      <p>Orloj u Divadla loutek se zastavil. Archivář Karel Kašpárek náhle odešel do důchodu – a zároveň zmizel. Spolu s ním zmizely i plány k opravě orloje, které nikdo jiný nezná. Bez nich orloj zůstane navždy tichý.</p>
-      <p>Vaším úkolem je zjistit, co se stalo, a vypátrat archivářovy stopy. Budete se pohybovat centrem Ostravy, hledat místa spojená s jeho minulostí, luštit hádanky, odemykat stopy a postupně skládat celý příběh.</p>
-      <h2>Co vás čeká:</h2>
-      <ul>
-        <li>Trasa měří přibližně 3 kilometry</li>
-        <li>Hra trvá kolem 90 minut</li>
-        <li>Stačí vám chytrý telefon s připojením k internetu</li>
-        <li>Trasa je vhodná pro kočárky</li>
-        <li>Začínáte i končíte u Divadla loutek Ostrava</li>
-        <p>Důležitá bude všímavost a týmová spolupráce. Sledujte detaily kolem sebe – některé indicie mohou být na první pohled nenápadné.</p>
+      <p><strong>Orloj u Divadla loutek se zastavil.</strong><br>
+Archivář Karel Kašpárek náhle odešel do důchodu – a zároveň zmizel. Spolu s ním zmizely i plány na opravu orloje, které nikdo jiný nezná. Bez nich zůstane orloj navždy tichý.</p>
 
-        <h2>Zahájení pátrání</h2>
+<p>Vaším úkolem je zjistit, co se stalo, a vypátrat archivářovy stopy. Budete se pohybovat centrem Ostravy, hledat místa spojená s jeho minulostí, luštit hádanky, odemykat stopy a postupně skládat celý příběh.</p>
 
-        <p>Právě se hlásíte jako expertní tým spolupracující s IT oddělením ostravské policie. Byl jste povoláni k případu ztraceného archiváře, protože dokážete číst mezi řádky, všímat si souvislostí a spojovat stopy. Během pátraní nás bude posouvat spousta hesel, ale žádne z nich neobsahuje mezery, na velikosti písmen nezáleží, ale na diakritice už ano.</p>
+<h2>Co vás čeká:</h2>
+<ul>
+  <li>Trasa měří přibližně 3 kilometry</li>
+  <li>Hra trvá zhruba 90 minut</li>
+  <li>Stačí vám chytrý telefon s připojením k internetu</li>
+  <li>Trasa je vhodná i pro kočárky</li>
+  <li>Začínáte i končíte u Divadla loutek Ostrava</li>
+  <li>Během pátrání budete zadávat různá <strong>hesla</strong> – žádné z nich neobsahuje <strong>mezery</strong>, <strong>nezáleží na velikosti písmen</strong>, ale <strong>diakritika je důležitá</strong></li>
+</ul>
 
-        <p>Policie lokalizovala archivářův telefon naposledy v okolí Divadla loutek. Je rozbitý a zamčený, ale může obsahovat zásadní vodítka. Nevíme však přesně, kde se nachází.</p>
+<p>Buďte všímaví a spolupracujte – některé indicie mohou být nenápadné, schované v detailech kolem vás.</p>
 
-        <p><strong>Vaším prvním úkolem je najít tento rozbitý telefon a opsat jaho sériové číslo.</strong></p>
+<h2>Zahájení pátrání</h2>
 
-      </ul>
+<p>Právě jste se přihlásili jako expertní tým spolupracující s IT oddělením ostravské policie. Byli jste povoláni k případu zmizelého archiváře – umíte číst mezi řádky, všímat si souvislostí a spojovat stopy.</p>
+
+<p>Policie naposledy lokalizovala archivářův telefon v okolí Divadla loutek. Je rozbitý a zamčený, ale může obsahovat zásadní vodítka. Přesné místo však zatím neznáme.</p>
+
+<p><strong>Vaším prvním úkolem je najít tento rozbitý telefon a opsat jeho sériové číslo.</strong></p>
+
+</div>
+<div>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -112,7 +119,6 @@ function LandingPage() {
           )}
         </div>
       </div></>
-  )
+     );
 }
-
 export default LandingPage;
